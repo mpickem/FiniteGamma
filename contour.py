1f291670a60c1eb96fd691b6fedb845bbc26cac4
Matthias Pickem <matthias.pickem@gmail.com>
2018-06-23 20:06:09 +0200

    Fixed colorbar in test-plot

--- contour.py
+++ contour.py
@@ -4,7 +4,6 @@ from __future__ import print_function, division, absolute_import
 import numpy as np
 import scipy.special
 import matplotlib.pyplot as plt
-import matplotlib.colorbar as clb
 
 
 x = np.linspace(-3,3,601)
@@ -16,8 +15,8 @@ betaeval = scipy.special.beta(xv,yv)
 
 g = plt.figure(figsize=(8,8))
 ax = g.add_axes([0.1,0.1,0.8,0.8])
-ax.pcolormesh(x,y,betaeval,vmin=-30,vmax=30)
+this = ax.pcolormesh(x,y,betaeval,vmin=-60,vmax=60, cmap='RdYlBu')
 
 cax = g.add_axes([0.91,0.1,0.02,0.8])
-clb.ColorbarBase(ax=cax)
+plt.colorbar(this, cax=cax)
 plt.show()
